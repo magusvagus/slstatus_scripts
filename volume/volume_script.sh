7afb6637e84c83a197166f906310252f05de65b9
magusvagus <magusvagus@proton.me>
2024-11-22 23:05:17 +0100

    cleaned up volume_script.sh, and added comments

--- volume/volume_script.sh
+++ volume/volume_script.sh
@@ -1,34 +1,15 @@
 #!/bin/bash
 
-## /bin/sh -c "amixer get Master | tail -n1 | grep -Po '\\[\\K[^%]*' | head -n1"
-
 vol="$(/bin/sh -c "amixer get Master | tail -n1 | grep -Po '\\[\\K[^%]*' | head -n1")"
 
 mute="$(amixer get Master | tail -n 1 | awk '{print $6}' | tr -d '[]')"
 
-yellow5="^c#b68642^"
-yellow4="^c#b27f42^" # yellow
-yellow3="^c#ae7843^"
-yellow2="^c#a86f43^"
-yellow1="^c#a56c43^"
-
-mid="^c#a36943^" # mid
-
-red1="^c#ac684a^"
-red2="^c#b36852^"
-red3="^c#bb675b^"
-red4="^c#c16765^"
-red5="^c#c76770^"
-
 block="^c#a36943^" # resets the color to normal
-crit="^c#924747^"
 crit2="^c#800000^"
 off="^c#626262^"
 
-# this works but the if statement dosent
-# (in other form, try out why)
-#if [ $vol -gt 50 ]; then echo "${red2}$vol${block}"; elif [ $vol -gt 60 ]; then echo "${blue}$vol${block}"; fi
-
+# Horrible, yes. But if -gt/ "<" is used the script dosent work,
+# right. Workaround needs to be found (AKA: rewriting this in C99)
 
 if   [[ "$mute" ==  "off" ]]; then
 	echo " ${off}--${block}"
